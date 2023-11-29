f81b3cad0f13ccea1d152f45e411dfcbd1110d3b
Emrah <emrahketzo@gmail.com>
2023-11-29 09:27:06 +0100

    First commit

---
+++ arborescence.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+touch index.html
+mkdir -p public/{css,pages,assets/{img,fonts},js,src/scss/modules}
+touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html && touch public/src/scss/app.scss && touch public/src/scss/_variables.scss && touch public/src/scss/_fonts.scss
+touch public/src/scss/modules/_style.scss && touch public/src/scss/modules/_header.scss && touch public/src/scss/modules/_nav.scss && touch public/src/scss/modules/_main.scss && touch public/src/scss/modules/_footer.scss && touch public/src/scss/modules/_colors.scss && touch public/src/scss/modules/_dimensions.scss && touch public/src/scss/modules/_buttons.scss
